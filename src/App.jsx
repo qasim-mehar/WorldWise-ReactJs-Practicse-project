13db88ae4778a8cdfc157f2322fd153c0e1057c6
Qasim <mianqasim065@gmail.com>
2025-08-07 17:45:02 +0500

    Add Route to PageNotFound page for exeptions

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import Homepage from './pages/homepage'
 import Pricing from './pages/Pricing'
 import Product from './pages/product'
+import PageNotFound from "./pages/PageNotFound"
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 export default function App() {
 
@@ -12,6 +13,7 @@ export default function App() {
            <Route path='/' element={<Homepage/>}> </Route>
            <Route path='product' element={<Product/>}> </Route>
            <Route path='pricing' element={<Pricing/>}> </Route>
+           <Route path='*' element={<PageNotFound/>}></Route>
         </Routes>
     </BrowserRouter>
   )
