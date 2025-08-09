46343219493704b313f02897d1cef66cab0aef21
Qasim <mianqasim065@gmail.com>
2025-08-09 19:17:31 +0500

    feat: make city list items clickable and navigate to city details page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import Homepage from './pages/homepage'
 import Pricing from './pages/Pricing'
 import Product from './pages/product'
 import Login from "./pages/Login"
 import PageNotFound from "./pages/PageNotFound"
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import AppLayout from './pages/AppLayout'
 import CityList from './components/CityList'
+import CountryList from './components/CountryList'
+import City from "./components/City"
 
 const BASE_UR="http://localhost:8000"
 export default function App() {
@@ -19,7 +21,7 @@ export default function App() {
     async function fetchCitiesData() {
       try{const res= await fetch(`${BASE_UR}/cities`);
       const data= await res.json();
-      console.log(data);
+      
       setCities(data)
       }
       catch{
@@ -39,9 +41,11 @@ export default function App() {
            <Route path='pricing' element={<Pricing/>}> </Route>
            <Route path='/app' element={<AppLayout/>}>
            //Note: Inintially we have to use a useState hook to set the currently active tab to decide what to show now we can do the same using routes as cities path is printing a different thing and countries path is diffrenet  
-             <Route element={<CityList isLoading={isLoading} cities={cities}/>}/>
+            <Route element={<CityList isLoading={isLoading} cities={cities}/>}/>
+             <Route index element={<CityList isLoading={isLoading} cities={cities}/>}></Route>
+             <Route path='cities/:id' element={<City/>}/>
              <Route path='cities' element={<CityList isLoading={isLoading} cities={cities}/>}></Route>
-             <Route path='countries' element={<p>List of Country</p>}></Route>
+             <Route path='countries' element={<CountryList cities={cities} isLoading={isLoading}/>}></Route>
            </Route>
            <Route path="/login" element={<Login/>}></Route>
            <Route path='*' element={<PageNotFound/>}></Route>
