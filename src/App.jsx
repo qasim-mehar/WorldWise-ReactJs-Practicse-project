3a9aebf778fd269794e872ce0f8dcc7318a57a17
Qasim <mianqasim065@gmail.com>
2025-08-10 13:43:00 +0500

    change: index route now redirects to 'cities' page, add 'cities' in URL

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 
 import Homepage from './pages/homepage'
 import Pricing from './pages/Pricing'
@@ -11,6 +11,7 @@ import CityList from './components/CityList'
 import CountryList from './components/CountryList'
 import City from "./components/City"
 import Form from "./components/Form"
+
 
 const BASE_UR="http://localhost:8000"
 export default function App() {
@@ -40,17 +41,17 @@ export default function App() {
            <Route index element={<Homepage/>}/> 
            <Route path='product' element={<Product/>}> </Route>
            <Route path='pricing' element={<Pricing/>}> </Route>
-           <Route path='app' element={<AppLayout/>}>
-           //Note: Inintially we have to use a useState hook to set the currently active tab to decide what to show now we can do the same using routes as cities path is printing a different thing and countries path is diffrenet  
-            <Route element={<CityList isLoading={isLoading} cities={cities}/>}/>
-             <Route index element={<CityList isLoading={isLoading} cities={cities}/>}></Route>
-             <Route path='cities/:id' element={<City/>}/>
-             <Route path='cities' element={<CityList isLoading={isLoading} cities={cities}/>}></Route>
-             <Route path='countries' element={<CountryList cities={cities} isLoading={isLoading}/>}></Route>
-             <Route path='form' element={<Form/>}></Route>
-           </Route>
            <Route path="/login" element={<Login/>}></Route>
            <Route path='*' element={<PageNotFound/>}></Route>
+          <Route path='app' element={<AppLayout/>}>
+             //Note: Inintially we have to use a useState hook to set the currently active tab to decide what to show now we can do the same using routes as cities path is printing a different thing and countries path is diffrenet  
+             <Route index element={<Navigate replace to='cities'/>}></Route>
+             <Route path='cities/:id' element={<City/>}/>
+             <Route path='cities' element={<CityList isLoading={isLoading} cities={cities}/>}></Route>
+             <Route element={<CityList isLoading={isLoading} cities={cities}/>}/>
+             <Route path='countries' element={<CountryList cities={cities} isLoading={isLoading}/>}></Route>
+             <Route path='form' element={<Form/>}></Route>
+          </Route>
         </Routes>
     </BrowserRouter>
   )
