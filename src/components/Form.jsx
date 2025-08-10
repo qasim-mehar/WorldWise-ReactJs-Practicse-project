7ae90e74e7e672755be5d4254752b8f53af684a0
Qasim <mianqasim065@gmail.com>
2025-08-10 13:26:33 +0500

    Feat: Create a Button component and add a back Button instance in Form to navigate user one step back using programatically navigation

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,8 +1,10 @@
 // "https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=0&longitude=0"
 
 import { useState } from "react";
-
 import styles from "./Form.module.css";
+import { useNavigate } from "react-router-dom";
+
+import Button from "./Button";
 
 export function convertToEmoji(countryCode) {
   const codePoints = countryCode
@@ -17,9 +19,11 @@ function Form() {
   const [country, setCountry] = useState("");
   const [date, setDate] = useState(new Date());
   const [notes, setNotes] = useState("");
+  const navigate= useNavigate()
 
   return (
     <form className={styles.form}>
+      
       <div className={styles.row}>
         <label htmlFor="cityName">City name</label>
         <input
@@ -49,8 +53,11 @@ function Form() {
       </div>
 
       <div className={styles.buttons}>
-        <button>Add</button>
-        <button>&larr; Back</button>
+        <Button type={'primary'}>Add</Button>
+        <Button onClick={(e)=>{
+          e.preventDefault();
+          navigate(-1)
+          }} type={'back'}>&larr; Back</Button>
       </div>
     </form>
   );
