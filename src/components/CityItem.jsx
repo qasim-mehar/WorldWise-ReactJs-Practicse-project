ae4f72f44a4fe77c1356191bfd1e0204762eb59e
Qasim <mianqasim065@gmail.com>
2025-08-12 20:26:49 +0500

    Feat: Highlight the currently selected city by applying a special className conditionally

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 import styles from './CityItem.module.css';
+import { useCities } from '../Contexts/CitiesContext';
   const formatDate = (date) =>
   new Intl.DateTimeFormat("en", {
     day: "numeric",
@@ -8,11 +9,12 @@ import styles from './CityItem.module.css';
   }).format(new Date(date));
 
 function CityItem({city}) {
-    const {cityName,emoji, date, id, position:{lat,lng}}=city
+   const {currentCity}=useCities()
+   const {cityName,emoji, date, id, position:{lat,lng}}=city
 
   return (
     <li >
-        <Link className={styles.cityItem} to={`${id}?lat=${lat}&lng=${lng}}`}>
+        <Link className={`${styles.cityItem} ${id===currentCity.id ? styles['cityItem--active'] : ""}`} to={`${id}?lat=${lat}&lng=${lng}}`}>
           <span className={styles.emoji}>{emoji}</span>
           <h3 className={styles.name}>{cityName}</h3>
           <time className={styles.date}>{formatDate(date)}</time>
