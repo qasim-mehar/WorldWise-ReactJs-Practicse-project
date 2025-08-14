ca7578fdf58463478e8bff37ca3ef83abdb670cc
Qasim <mianqasim065@gmail.com>
2025-08-14 20:53:31 +0500

    Fix: Change map theme and set ScrollWheelZoon to true to zoom map by scrolling

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -14,18 +14,17 @@ function Map() {
 
   return (
     <div className={styles.mapContainer} onClick={()=>navigate("form")}>
-     <MapContainer className={styles.map} center={mapPosition} zoom={13} scrollWheelZoom={false}>
+     <MapContainer className={styles.map} center={mapPosition} zoom={13} scrollWheelZoom={true}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
         />
         <Marker position={mapPosition}>
           <Popup>
             A pretty CSS3 popup. <br /> Easily customizable.
           </Popup>
         </Marker>
-      </MapContainer>,
-
+      </MapContainer>
     </div>
   );
 }
