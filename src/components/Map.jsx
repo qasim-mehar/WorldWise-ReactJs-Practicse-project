dcbf87661764d51c49506e0939415eea9b1c3894
Qasim <mianqasim065@gmail.com>
2025-08-17 19:37:26 +0500

    Feat: Center the marker of currenctly selected city

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,7 +1,7 @@
 import { useSearchParams, useNavigate } from 'react-router-dom';
 import {MapContainer,  TileLayer,Marker,Popup,useMap,} from 'react-leaflet'
 import styles from './Map.module.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useCities } from '../Contexts/CitiesContext';
 
 
@@ -9,15 +9,18 @@ function Map() {
   const [mapPosition, setMapPossition] =useState([40, 0]);
   const {cities} =useCities();
   const navigate= useNavigate()
-  const [searchParam, setSearchParam]= useSearchParams();
-  const lat=searchParam.get('lat');
-  const lng=searchParam.get('lng');
+  const [searchParam]= useSearchParams();
+  const mapLat=searchParam.get('lat');
+  const mapLng=searchParam.get('lng');
 
+  useEffect(function(){
+    if(mapLat || mapLng) setMapPossition([parseFloat(mapLat), parseFloat(mapLng)]);
+  },[mapLat,mapLng])
 
 
   return (
     <div className={styles.mapContainer} onClick={()=>navigate("form")}>
-     <MapContainer className={styles.map} center={mapPosition} zoom={13} scrollWheelZoom={true}>
+     <MapContainer className={styles.map} center={mapPosition} zoom={6} scrollWheelZoom={true}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
@@ -27,9 +30,15 @@ function Map() {
             <span>{city.emoji}</span> <span>{city.cityName}</span>
           </Popup>
         </Marker>)}
+        <ChangeCenter position={mapPosition}/>
       </MapContainer>
     </div>
   );
+}
+function ChangeCenter({position}){
+  const map =useMap();
+  map.setView(position)
+  return null;
 }
 
 export default Map;
