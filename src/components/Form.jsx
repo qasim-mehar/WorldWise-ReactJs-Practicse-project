16d92ad985ff418e4cc708432b0ccbf0d971c1ae
Qasim <mianqasim065@gmail.com>
2025-08-18 19:35:09 +0500

    Feat: Imporn a new DatePicker component to pick a date on Form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,9 @@
 // "https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=0&longitude=0"
 
 import { useEffect, useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
 import styles from "./Form.module.css";
 import { useNavigate } from "react-router-dom";
 import { useURLPosition } from "../../Hooks/useURLPosition";
@@ -76,11 +79,12 @@ function Form() {
 
       <div className={styles.row}>
         <label htmlFor="date">When did you go to {cityName}?</label>
-        <input
+        {/* <input
           id="date"
           onChange={(e) => setDate(e.target.value)}
           value={date}
-        />
+        /> */}
+        <DatePicker selected={date} onChange={date=>setDate(date)}/>
       </div>
 
       <div className={styles.row}>
