226ab4ff37cf903aafa4687bc0cc6b3878dc90f4
Qasim <mianqasim065@gmail.com>
2025-08-23 02:45:41 +0500

    Feat: Implement lazy loading for all the pages to splitt code for better optimization

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,35 @@
+import { lazy ,Suspense} from 'react'
 import { BrowserRouter, Route, Routes,Navigate } from 'react-router-dom'
 
 import { CitiesProvider } from './Contexts/CitiesContext'
 import { AuthProvider } from './Contexts/FakeAuthContext'
-import Homepage from './pages/homepage'
-import Pricing from './pages/Pricing'
-import Product from './pages/product'
-import Login from "./pages/Login"
-import PageNotFound from "./pages/PageNotFound"
-import AppLayout from './pages/AppLayout'
+
 import CityList from './components/CityList'
 import CountryList from './components/CountryList'
 import City from "./components/City"
 import Form from "./components/Form"
 import ProtectAuthentication from './pages/ProtectAuthentication'
+import SpinnerFullPage from "./components/SpinnerFullPage"
 
+const PageNotFound=lazy(()=>import("./pages/PageNotFound"))
+const Pricing= lazy(()=>import('./pages/Pricing'))
+const Login=lazy(()=>import("./pages/Login"))
+const Product= lazy(()=>import('./pages/product'))
+const AppLayout=lazy(()=>import('./pages/AppLayout'))
+const Homepage=lazy(()=>import('./pages/homepage'))
+// import PageNotFound from "./pages/PageNotFound"
+// import Pricing from './pages/Pricing'
+// import Login from "./pages/Login"
+// import Product from './pages/product'
+// import AppLayout from './pages/AppLayout'
+// import Homepage from './pages/homepage'
 
 export default function App() {
   return (
     <AuthProvider>
       <CitiesProvider>
       <BrowserRouter>
+         <Suspense fallback={<SpinnerFullPage/>}>
           <Routes>
             <Route index element={<Homepage/>}/> 
             <Route path='product' element={<Product/>}> </Route>
@@ -40,6 +50,7 @@ export default function App() {
               <Route path='form' element={<Form/>}></Route>
             </Route>
           </Routes>
+         </Suspense> 
       </BrowserRouter>
       </CitiesProvider>
       </AuthProvider>
